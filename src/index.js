146cb576e8b0dbfaf15d9b9dc4d0fc0d8134507d
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-06-23 20:10:17 -0300

    adicao do post de restaurantes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
+
+const restaurantes = []
 
 app.get('/', (req, res) => {
   res.send('IFood API')
+})
+
+app.post('/restaurante', (req, res) => {
+  const { nome, endereco } = req.body
+
+  const novoRestaurante = {
+    id: restaurantes.length + 1,
+    nome: nome,
+    endereco: endereco,
+    cardapio: []
+  }
+
+  restaurantes.push(novoRestaurante)
+
+  res.status(201).json(novoRestaurante)
 })
 
 app.listen(3333, () => {
