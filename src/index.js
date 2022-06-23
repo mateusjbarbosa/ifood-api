6a3424a631d7acb38333391dfbcf4299caf05cc2
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-06-23 20:45:35 -0300

    adicionado post de prato no cardapio

--- src/index.js
+++ src/index.js
@@ -28,6 +28,30 @@ app.get('/restaurantes', (req, res) => {
   res.send(restaurantes)
 })
 
+
+app.post('/restaurantes/:id/cardapio', (req, res) => {
+  const { id } = req.params
+  const { nome, preco } = req.body
+
+  const restauranteSelecionado = restaurantes.find(restaurante => restaurante.id === Number(id))
+
+  if (!restauranteSelecionado) {
+    return res.status(401).json({
+      erro: "Restaurante não encontrado"
+    })
+  }
+
+  const novoPrato = {
+    id: restauranteSelecionado.cardapio.length + 1,
+    nome: nome,
+    preco: preco
+  }
+
+  restauranteSelecionado.cardapio.push(novoPrato)
+
+  res.status(201).json(novoPrato)
+})
+
 app.listen(3333, () => {
   console.log("API rodando na porta 3333")
 })
